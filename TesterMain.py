83a038864d035b7d06d743634538a34fa61c5670
AlexGruPerm <ugr@bk.ru>
2017-10-27 20:04:45 +0500

    Calculation count of data columns and sum one column for compare total with test property.

--- TesterMain.py
+++ TesterMain.py
@@ -2,6 +2,9 @@ from PageDownloader import PageDownload
 from FXpathSeacher import XpathSearch
 import requests
 import Params
+import json
+import pprint
+from decimal import Decimal
 
 #AlexGruPerm github
 #https://github.com/AlexGruPerm/web_tester.git
@@ -29,7 +32,60 @@ url = 'http://mkrpk.ders.proitr.ru/salary/index.php'
 #report_grid.get_content(url,Params.grid_param_3,True)
 report_grid.read_content_file()
 
+
+
 grid_search = XpathSearch(report_grid.content)
-grid_search.search("//*")
-#Comments for test git from PyCharm into github _
+print(type(grid_search))
+r = grid_search.search("//table/tbody/tr",False)
+r_cnt = len(r)-1
+#print("tr count = ",r_cnt)
+matrix = []#[[0 for i in range(1)] for j in range(1)] # column with [0] count = count of <tr>
+
+# ТЕСТ это сумма первого столбца.
+
+
+# ищем таблицу и считаем в ней количество строк и колонок и среди них пустые !!!
+print(type(r))
+table_tr_count = 0
+for trs in r[1:len(r)]:
+ table_tr_count += 1
+ #grid_search.print_html_elm(trs)
+ #print('----------------------------------')
+ table_line = []
+ for t in trs:
+     #grid_search.print_html_elm(t)
+     #print(str(ths.text))
+     if t.tag=='td':
+         table_line.append(str(t.text))
+ #print(table_line)
+ matrix.append(table_line)
+#print("Table contains : ",str(table_tr_count)," rows.")
+
+'''
+print(type(Params.grid_param_3))
+udata = json.dumps(Params.grid_param_3, sort_keys=False,indent=4, separators=(',', ': '))
+print(type(udata))
+
+if 'data' in udata:
+    print('Exists search Data!')
+else:
+    print('No search Data!')
+'''
+print("OK 1. len_matric=",len(matrix))
+
+test_col_num = 0 # first column in report
+test_col_sum = Decimal(0.0)
+
+for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                if j==0:
+                    try:
+                        test_col_sum = test_col_sum + Decimal(matrix[i][j].strip().replace(',', '.').replace(u'\xa0',''))
+                    except ValueError:
+                        print("      error on line [",i,"]",end=" ")
+
+print("OK 2. test_col_sum=",str(test_col_sum))
+
+
+#pprint.pprint(matrix)
 
