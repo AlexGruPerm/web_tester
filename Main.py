0c02c9b299fa28488f4bff0923845ff45dfb51f4
AlexGruPerm <ugr@bk.ru>
2017-10-31 15:27:34 +0500

    Logical point

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from FWTest_Rep_11_1 import WTest_Rep_11_1
 
 def main():
     """ Main procedure to start all tests. """
-    tr = TestRunner([WTest_Rep_11_1,WTest_Rep_01_1])
+    tr = TestRunner([WTest_Rep_11_1, WTest_Rep_01_1])
     tr.init_test()
     tr.start()
     print('=======================')
