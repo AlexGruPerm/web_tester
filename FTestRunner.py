192627e4344dee2e0740c183f5818cd4ee329754
AlexGruPerm <ugr@bk.ru>
2017-11-02 17:01:37 +0500

    FTestRunner checked and fixed with Pylint

--- FTestRunner.py
+++ FTestRunner.py
@@ -1,26 +1,29 @@
+"""Class TestRunner create's instances of all test class and run start() """
 
 class TestRunner():
-    """ Class for runs individual test from input list"""
+    '''Class for runs individual test from input list'''
     arr_test_objs = []   # list of test objects
     arr_test_names = []  # list of test class names
 
-    def __init__(self,tests_list):
-        """ init property arr_test_names with input classes (no class names) """
+    def __init__(self, tests_list):
+        '''init property arr_test_names with input classes (no class names)'''
         print('Tests begins.')
         self.arr_test_names = tests_list # list
 
     def init_test(self):
-        """ Go through test class names and create instances of this Classes. Populate list of objects. """
+        '''Go through test class names and create instances of this Classes.
+           Populate list of objects.'''
         for i in range(len(self.arr_test_names)):
-            self.arr_test_objs.append(self.arr_test_names[i]())  # create instance of class and append it into list
+            self.arr_test_objs.append(self.arr_test_names[i]())  # create instance of class
 
     def start(self):
-        """ Go through list of test objects and start each of them. """
+        '''Go through list of test objects and start each of them.'''
         for i in range(len(self.arr_test_objs)):
-            self.arr_test_objs[i].start()  # for each instance is running start method that defined in parent class TestProject
+            self.arr_test_objs[i].start()  # running start method that defined in TestProject
         print('Tests ends.')
 
     def test_arr_objs(self):
-        """ make some test about array of objects"""
+        '''make some test about array of objects'''
         for i in range(len(self.arr_test_objs)):
-            print(self.arr_test_objs[i].test_name," was loaded ",self.arr_test_objs[i].download_time," seconds.")
+            print(self.arr_test_objs[i].test_name, " was loaded ",
+                  self.arr_test_objs[i].download_time, " seconds.")
