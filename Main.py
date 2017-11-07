bed278630d1b8913dfa7157c05090b2c20951355
AlexGruPerm <ugr@bk.ru>
2017-11-07 17:05:20 +0500

    Tests now in the common list.

--- Main.py
+++ Main.py
@@ -3,11 +3,14 @@ import logging
 from FTestRunner import TestRunner
 from FWTest_Rep_11_1_v2 import WTest_Rep_11_1_v2
 
-logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
+logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.DEBUG)
 
 def main():
     '''Main procedure to start all tests.'''
-    test_runner = TestRunner([WTest_Rep_11_1_v2])
+    tests_list = []
+    # here we can add tests into common list
+    tests_list.append(WTest_Rep_11_1_v2)
+    test_runner = TestRunner(tests_list)
     #WTest_Rep_11_1, WTest_Rep_11_1_v2, WTest_Rep_01_1
     test_runner.init_test()
     test_runner.start()
